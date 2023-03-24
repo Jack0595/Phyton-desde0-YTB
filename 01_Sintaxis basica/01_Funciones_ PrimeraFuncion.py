7ae81212c432e7b0c24c713cf6fde194e6070e13
JackLop <bob_mortal@hotmail.com>
2023-03-23 23:16:09 -0600

    Se carga el primer Ejercicio, e inicia el repositorio

---
+++ 01_Sintaxis basica/01_Funciones_ PrimeraFuncion.py
@@ -0,0 +1,12 @@
+def mensaje():
+	print("Estamos aprendiendo Python")
+	print("Estamos aprendiendo intrsucciones basicas")
+	print("Poco a poco iremos avanzando")
+
+mensaje()
+
+mensaje ()
+
+print("Ejecutando codigo fuera de funcion")
+
+mensaje()
